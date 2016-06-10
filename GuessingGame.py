a9d9027a18eee97a1fe0687b8bc0ae35f58ad7dc
atauf <ataufmann@gmail.com>
2016-06-10 10:59:36 -1000

    Added ability for user to create upper and lower bounds

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,13 +1,17 @@
 from random import randint
 
-num = randint(1, 100)
-print (num)  #For Testing...comment this line out if you're actually playing the game.  
-print ("Let's play a guessing game...You'll get 5 tries to pick a number between 1 and 100!  Good Luck!")
+print ("Let's play a guessing game...You'll get 5 tries to pick a number between\
+ a range of numbers that you give me.  Good Luck!")
+print ("You tell me what range you want to have to pick a number from...")
+lwrBound = input("Lower bound: ")
+upprBound = input("Upper bound: ")
+num = randint(int(lwrBound), int(upprBound))
+print (num)  #For Testing...comment this line out if you're actually playing the game.
 count = 0
 
 while count < 5:
     print ("Attempt #%d" % (count + 1))
-    guess = input("Enter a number between 1 and 100: ")
+    guess = input("Enter a number between %s and %s: " % (lwrBound, upprBound))
     # print ("Guess: %s" % (guess))
     # print ("Num: %d" % (num))
     if guess == str(num):
@@ -20,8 +24,8 @@ while count < 5:
             if playAgain[0].lower() == "y":
                 print ("Ok, here we go again!\n5 more tries!\n")
                 count = 0
-                num = randint(1, 100)
-                print ("New number to guess is: %d" % (num))
+                num = randint(int(lwrBound), int(upprBound))
+                print ("New number to guess is: %d" % (num)) #For Testing...comment this line out if you're actually playing the game.
                 continue
             elif playAgain[0].lower() == "n":
                 print ("Alright, catch you on the flip side!")
